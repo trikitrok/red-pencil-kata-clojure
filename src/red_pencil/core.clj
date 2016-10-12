afed7911761e379304733c741d8665d7fee5eb25
trikitrok <trikitrok@gmail.com>
2016-10-12 06:59:46 +0200

    price reductions activate promotions

--- src/red_pencil/core.clj
+++ src/red_pencil/core.clj
@@ -1,4 +1,7 @@
 (ns red-pencil.core)
 
 (defn change-price [good new-price]
-  (assoc good :price new-price))
+  (let [price (:price good)]
+    (if (< new-price price)
+      (merge good {:price new-price :on-promotion true})
+      (assoc good :price new-price))))
--- test/red_pencil/core_test.clj
+++ test/red_pencil/core_test.clj
@@ -9,4 +9,10 @@
   (fact
     "price rises don't generate any promotion"
     (let [good {:price 200 :on-promotion false}]
-      (red-pencil/change-price good 250) => {:price 250 :on-promotion false})))
+      (red-pencil/change-price good 250) => {:price 250 :on-promotion false}))
+
+  (facts
+    "price reductions generate promotions"
+
+    (let [good {:price 100 :on-promotion false}]
+      (red-pencil/change-price good 90) => {:price 90 :on-promotion true})))
