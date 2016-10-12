6504f1548962233225fbe37b6fcb0022844fb73a
trikitrok <trikitrok@gmail.com>
2016-10-12 08:38:11 +0200

    extract time manipulation to a separate namespace

--- src/red_pencil/core.clj
+++ src/red_pencil/core.clj
@@ -1,11 +1,14 @@
-(ns red-pencil.core)
+(ns red-pencil.core
+  (:require
+    [red-pencil.days :as days]))
 
 (defn- reduction-ratio [price new-price]
   (/ (- price new-price) price))
 
 (def ^:private reduction-ratio-range [0.05 0.3])
 
-(defn- price-reduction-in-range? [{price :figure} {new-price :figure} [min-reduction-ratio max-reduction-ratio]]
+(defn- price-reduction-in-range?
+  [{price :figure} {new-price :figure} [min-reduction-ratio max-reduction-ratio]]
   (let [reduction-ratio (reduction-ratio price new-price)]
     (and (>= reduction-ratio min-reduction-ratio)
          (<= reduction-ratio max-reduction-ratio))))
@@ -13,9 +16,11 @@
 (defn- price-reduction? [{price :figure} {new-price :figure}]
   (< new-price price))
 
-(defn- old-price-stable-enough? [{old-price-ts :change-ts} {new-price-ts :change-ts}]
-  (let [ms-in-day (* 24 60 60 1000)]
-    (> (/ (- new-price-ts old-price-ts) ms-in-day) 30)))
+(defn- price-duration-in-days [{old-price-ts :change-ts} {new-price-ts :change-ts}]
+  (days/from-ms (- new-price-ts old-price-ts)))
+
+(defn- old-price-stable-enough? [price new-price]
+  (> (price-duration-in-days price new-price) 30))
 
 (defn- on-promotion? [{:keys [price]} new-price]
   (and (price-reduction? price new-price)
---
+++ src/red_pencil/days.clj
@@ -0,0 +1,10 @@
+(ns red-pencil.days)
+
+(def ^:private ms-in-day (* 24 60 60 1000))
+
+(defn- to-ms [num-days]
+  (* num-days ms-in-day))
+
+(defn from-ms [ms]
+  (/ ms ms-in-day))
+
