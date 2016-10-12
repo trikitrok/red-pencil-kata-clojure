17e74ebff9aea456747038e7b27d331396c92963
trikitrok <trikitrok@gmail.com>
2016-10-12 07:04:13 +0200

    extract helper function

--- src/red_pencil/core.clj
+++ src/red_pencil/core.clj
@@ -1,7 +1,9 @@
 (ns red-pencil.core)
 
+(defn- on-promotion? [{:keys [price]} new-price]
+  (< new-price price))
+
 (defn change-price [good new-price]
-  (let [price (:price good)]
-    (if (< new-price price)
-      (merge good {:price new-price :on-promotion true})
-      (assoc good :price new-price))))
+  (-> good
+      (assoc :price new-price)
+      (assoc :on-promotion (on-promotion? good new-price))))
