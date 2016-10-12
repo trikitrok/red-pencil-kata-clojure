33039dc2df0264f056ee93f7782dbd1d53513e59
trikitrok <trikitrok@gmail.com>
2016-10-12 10:59:23 +0200

    rename some parameters and functions

--- src/red_pencil/promotions_identification.clj
+++ src/red_pencil/promotions_identification.clj
@@ -8,38 +8,38 @@
 
 (def ^:private maximum-promotion-duration 30)
 
-(defn- reduction-ratio [old-price price]
-  (/ (- old-price price) old-price))
+(defn- reduction-ratio [previous-price price]
+  (/ (- previous-price price) previous-price))
 
 (defn- price-reduction-in-range?
-  [{old-price :figure} {price :figure} [min-reduction-ratio max-reduction-ratio]]
-  (let [reduction-ratio (reduction-ratio old-price price)]
+  [{previous-price :figure} {price :figure} [min-reduction-ratio max-reduction-ratio]]
+  (let [reduction-ratio (reduction-ratio previous-price price)]
     (and (>= reduction-ratio min-reduction-ratio)
          (<= reduction-ratio max-reduction-ratio))))
 
-(defn- price-reduction? [{old-price :figure} {price :figure}]
-  (< price old-price))
+(defn- price-reduction? [{previous-price :figure} {price :figure}]
+  (< price previous-price))
 
-(defn- price-duration-in-days [{old-price-ts :change-ts} {new-price-ts :change-ts}]
-  (days/from-ms (- new-price-ts old-price-ts)))
+(defn- price-duration-in-days [{previous-price-ts :change-ts} {new-price-ts :change-ts}]
+  (days/from-ms (- new-price-ts previous-price-ts)))
 
-(defn- old-price-stable-enough? [old-price price]
-  (>= (price-duration-in-days old-price price) minimum-price-duration))
+(defn- previous-price-stable-enough? [previous-price price]
+  (>= (price-duration-in-days previous-price price) minimum-price-duration))
 
 (defn- promotion-still-lasts? [{:keys [change-ts]} query-ts]
   (<= (days/from-ms (- query-ts change-ts)) maximum-promotion-duration))
 
-(defn- price-before-old-price [good]
+(defn- price-before-previous-price [good]
   (->> good :previous-prices (take-last 2) first))
 
-(defn- activates-promotion? [old-price price query-ts]
-  (and (price-reduction? old-price price)
-       (price-reduction-in-range? old-price price reduction-ratio-range)
+(defn- activates-promotion? [previous-price price query-ts]
+  (and (price-reduction? previous-price price)
+       (price-reduction-in-range? previous-price price reduction-ratio-range)
        (promotion-still-lasts? price query-ts)))
 
 (defn on-promotion? [good query-ts]
   (let [price (:price good)
-        old-price (-> good :previous-prices last)]
-    (if (old-price-stable-enough? old-price price)
-      (activates-promotion? old-price price query-ts)
-      (activates-promotion? (price-before-old-price good) old-price query-ts))))
+        previous-price (-> good :previous-prices last)]
+    (if (previous-price-stable-enough? previous-price price)
+      (activates-promotion? previous-price price query-ts)
+      (activates-promotion? (price-before-previous-price good) previous-price query-ts))))
