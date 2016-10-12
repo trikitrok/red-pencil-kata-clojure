a75fa207cd746cb24b36ac37f1f04dc40429ba93
trikitrok <trikitrok@gmail.com>
2016-10-12 07:37:32 +0200

    encapsulate price reads and writes in order to be able to pass it from a number to an object

--- src/red_pencil/core.clj
+++ src/red_pencil/core.clj
@@ -3,6 +3,12 @@
 (defn- reduction-ratio [price new-price]
   (/ (- price new-price) price))
 
+(defn- get-price-figure [price]
+  price)
+
+(defn- update-price-figure [new-price price]
+  new-price)
+
 (def ^:private reduction-ratio-range [0.05 0.3])
 
 (defn- reduction-in-range? [price new-price [min-reduction-ratio max-reduction-ratio]]
@@ -11,10 +17,10 @@
          (<= reduction-ratio max-reduction-ratio))))
 
 (defn- on-promotion? [{:keys [price]} new-price]
-  (and (< new-price price)
-       (reduction-in-range? price new-price reduction-ratio-range)))
+  (and (< new-price (get-price-figure price))
+       (reduction-in-range? (get-price-figure price) new-price reduction-ratio-range)))
 
 (defn change-price [good new-price]
   (-> good
-      (assoc :price new-price)
+      (update :price (partial update-price-figure new-price))
       (assoc :on-promotion (on-promotion? good new-price))))
--- test/red_pencil/core_test.clj
+++ test/red_pencil/core_test.clj
@@ -3,24 +3,27 @@
     [midje.sweet :refer :all]
     [red-pencil.core :as red-pencil]))
 
+(defn- price [figure]
+  figure)
+
 (facts
   "about red pencil promotions"
 
   (fact
     "price rises don't activate any promotion"
-    (let [good {:price 200 :on-promotion false}]
-      (red-pencil/change-price good 250) => {:price 250 :on-promotion false}))
+    (let [good {:price (price 200) :on-promotion false}]
+      (red-pencil/change-price good 250) => {:price (price 250) :on-promotion false}))
 
   (facts
     "price reductions activate promotions"
-    (let [good {:price 100 :on-promotion false}]
-      (red-pencil/change-price good 95) => {:price 95 :on-promotion true}
-      (red-pencil/change-price good 70) => {:price 70 :on-promotion true}
+    (let [good {:price (price 100) :on-promotion false}]
+      (red-pencil/change-price good 95) => {:price (price 95) :on-promotion true}
+      (red-pencil/change-price good 70) => {:price (price 70) :on-promotion true}
 
       (fact
         "except when the price is reduced less than 5%"
-        (red-pencil/change-price good 98) => {:price 98 :on-promotion false})
+        (red-pencil/change-price good 98) => {:price (price 98) :on-promotion false})
 
       (fact
         "except when the price is reduced more than 30%"
-        (red-pencil/change-price good 69) => {:price 69 :on-promotion false}))))
+        (red-pencil/change-price good 69) => {:price (price 69) :on-promotion false}))))
