9447546178d8833db502e569b9a0cde242bad668
trikitrok <trikitrok@gmail.com>
2016-10-12 09:55:06 +0200

    explain better the limit cases

--- test/red_pencil/promotions_identification_tests.clj
+++ test/red_pencil/promotions_identification_tests.clj
@@ -24,21 +24,43 @@
     "price reductions activate promotions"
 
     (let [previous-price (price 100 (days/to-ms 0))
+          price-reduction-upper-limit 95
           price-change-ts (days/to-ms 55)
-          price-reduced-5-percent (price 95 price-change-ts)
+          price-reduced-5-percent (price price-reduction-upper-limit price-change-ts)
           good {:price price-reduced-5-percent :previous-prices [previous-price]}
           query-ts (+ price-change-ts (days/to-ms 5))]
       (promotions-identification/on-promotion? good query-ts) => true)
 
     (let [previous-price (price 100 (days/to-ms 0))
+          price-reduction-lower-limit 70
           price-change-ts (days/to-ms 30)
-          price-reduced-30-percent (price 70 price-change-ts)
+          price-reduced-30-percent (price price-reduction-lower-limit price-change-ts)
           good {:price price-reduced-30-percent :previous-prices [previous-price]}
           query-ts (+ price-change-ts (days/to-ms 4))]
       (promotions-identification/on-promotion? good query-ts) => true)
 
+    (let [previosu-price-ts (days/to-ms 0)
+          previous-price (price 100 previosu-price-ts)
+          previous-price-maximum-duration-in-days 30
+          price-change-ts (+ previosu-price-ts (days/to-ms previous-price-maximum-duration-in-days))
+          price-reduced-30-percent (price 90 price-change-ts)
+          good {:price price-reduced-30-percent :previous-prices [previous-price]}
+          query-ts (+ price-change-ts (days/to-ms 4))]
+      (promotions-identification/on-promotion? good query-ts) => true)
+
+    (let [previous-price (price 100 (days/to-ms 0))
+          price-change-ts (days/to-ms 30)
+          price-reduced-30-percent (price 80 price-change-ts)
+          good {:price price-reduced-30-percent :previous-prices [previous-price]}
+          promotion-maximum-duration-in-days 30
+          query-ts (+ price-change-ts (days/to-ms promotion-maximum-duration-in-days))]
+      (promotions-identification/on-promotion? good query-ts) => true))
+
+  (facts
+    "price reductions don't activate promotions"
+
     (fact
-      "except when the price is reduced less than 5%"
+      "when the price is reduced less than 5%"
       (let [previous-price (price 100 (days/to-ms 0))
             price-change-ts (days/to-ms 45)
             new-price (price 98 price-change-ts)
@@ -47,7 +69,7 @@
         (promotions-identification/on-promotion? good query-ts) => false))
 
     (fact
-      "except when the price is reduced more than 30%"
+      "when the price is reduced more than 30%"
       (let [previous-price (price 100 (days/to-ms 0))
             price-change-ts (days/to-ms 31)
             new-price (price 69 price-change-ts)
@@ -56,7 +78,7 @@
         (promotions-identification/on-promotion? good query-ts) => false))
 
     (fact
-      "except when the previous price has been stable for less than 30 days"
+      "when the previous price has been stable for less than 30 days"
       (let [previous-price (price 100 (days/to-ms 0))
             price-change-ts (days/to-ms 10)
             new-price (price 80 price-change-ts)
@@ -65,7 +87,7 @@
         (promotions-identification/on-promotion? good query-ts) => false))
 
     (fact
-      "except when the price changed was more than 30 days"
+      "when the price changed was more than 30 days"
       (let [previous-price (price 100 (days/to-ms 0))
             price-change-ts (days/to-ms 30)
             new-price (price 80 price-change-ts)
