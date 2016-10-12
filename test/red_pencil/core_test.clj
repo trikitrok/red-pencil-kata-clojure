505a56b07c5eac4a9f4b65cc04811bdbbd62067c
trikitrok <trikitrok@gmail.com>
2016-10-12 07:00:46 +0200

    rename to use language closer to the domain in tests descriptions

--- test/red_pencil/core_test.clj
+++ test/red_pencil/core_test.clj
@@ -7,12 +7,12 @@
   "about red pencil promotions"
 
   (fact
-    "price rises don't generate any promotion"
+    "price rises don't activate any promotion"
     (let [good {:price 200 :on-promotion false}]
       (red-pencil/change-price good 250) => {:price 250 :on-promotion false}))
 
   (facts
-    "price reductions generate promotions"
+    "price reductions activate promotions"
 
     (let [good {:price 100 :on-promotion false}]
       (red-pencil/change-price good 90) => {:price 90 :on-promotion true})))
