25731d1d1dfeec918b74a48e12288f185f5cd9d1
trikitrok <trikitrok@gmail.com>
2016-10-12 06:54:07 +0200

    price rises don't generate any promotion

--- src/red_pencil/core.clj
+++ src/red_pencil/core.clj
@@ -1,1 +1,4 @@
 (ns red-pencil.core)
+
+(defn change-price [good new-price]
+  (assoc good :price new-price))
--- test/red_pencil/core_test.clj
+++ test/red_pencil/core_test.clj
@@ -1,3 +1,12 @@
 (ns red-pencil.core-test
   (:require
-    [midje.sweet :refer :all]))
+    [midje.sweet :refer :all]
+    [red-pencil.core :as red-pencil]))
+
+(facts
+  "about red pencil promotions"
+
+  (fact
+    "price rises don't generate any promotion"
+    (let [good {:price 200 :on-promotion false}]
+      (red-pencil/change-price good 250) => {:price 250 :on-promotion false})))
